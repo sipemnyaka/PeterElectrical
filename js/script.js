78585beb8fba144228fc2ffbcd773e6bdfbef3fa
Siphe <sipemnyaka@gmail.com>
2026-07-17 01:07:14 +0200

    Added and styled Services, About, and Contact section

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const navLinks = document.querySelectorAll("nav ul a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    let currentSection = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop - 120) {
+            currentSection = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + currentSection) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
